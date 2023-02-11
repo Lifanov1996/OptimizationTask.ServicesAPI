5919d68b24796b6c6c3a7ed63fb27749cd004329
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-02-11 17:59:20 +0300

    Добавлены модели БД

--- ServicesAPI/Data/Entity/ContextDB.cs
+++ ServicesAPI/Data/Entity/ContextDB.cs
@@ -1,9 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ServicesAPI.Models.Applications;
+using ServicesAPI.Models.Contacts;
+using ServicesAPI.Models.Headers;
+using ServicesAPI.Models.News;
+using ServicesAPI.Models.Projects;
+using ServicesAPI.Models.Services;
 using ServicesAPI.Models.Users;
 
 namespace ServicesAPI.Data.Entity
 {
     public class ContextDB : IdentityDbContext<User>
     {
+        public DbSet<Applications> Applications { get; set; }
+        public DbSet<Contacts> Contacts { get; set; }
+        public DbSet<Headers> Headers { get; set; }
+        public DbSet<News> News { get; set; }
+        public DbSet<Projects> Projects { get; set; }
+        public DbSet<Services> Services { get; set; }
+        
+        public ContextDB(DbContextOptions options) :base(options) { }
     }
 }
