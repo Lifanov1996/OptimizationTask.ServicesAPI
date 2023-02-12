0c548325eac378181d739eef77f0a477857752f4
Evgeny Lifanov <lifanov.j@yandex.ru>
2023-02-12 15:38:59 +0300

    Добавление интерфейса заявок

---
+++ ServicesAPI/BusinessLogic/Contracts/IApplicationAdmin.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServicesAPI.Models.Applications;
+
+namespace ServicesAPI.BusinessLogic.Contracts
+{
+    public interface IApplicationAdmin
+    {
+        /// <summary>
+        /// Получение одной заявки
+        /// </summary>
+        /// <param name="appId">Номер заявки</param>
+        /// <returns></returns>
+        Task<Applications> SeeOneApp(string appId);
+
+        /// <summary>
+        /// Получение всех заявкок
+        /// </summary>
+        /// <returns></returns>
+        Task<List<Applications>> SeeAllApp();
+
+        /// <summary>
+        /// Изменение статуса заявки
+        /// </summary>
+        /// <param name="applications"></param>
+        /// <returns></returns>
+        Task ChangStatusApp(Applications applications);
+
+        /// <summary>
+        /// Удаление заявки 
+        /// </summary>
+        /// <param name="appId">Номер заявки</param>
+        /// <returns></returns>
+        Task DeleteApp(string appId);
+    }
+}
---
+++ ServicesAPI/BusinessLogic/Contracts/IApplicationClient.cs
@@ -0,0 +1,13 @@
+﻿namespace ServicesAPI.BusinessLogic.Contracts
+{
+    public interface IApplicationClient
+    {
+        /// <summary>
+        /// Отправка заявки клиентом
+        /// </summary>
+        /// <param name="NameCl">Имя клиента</param>
+        /// <param name="Description">Сообщение заявки</param>
+        /// <param name="Email">Элетронная почта клиента</param>
+        Task SendAppClient(string NameCl, string Description, string Email);
+    }
+}
